dcac5c1c1715025dc0270ada972594d266ae7767
Jose A Quijada <jaquijada@localhost.localdomain>
2019-06-06 19:33:30 -0700

    Add sum logic, controller and unit test

---
+++ src/main/java/com/jaquijada/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.jaquijada.calculator;
+//import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+   
+/**
+ * Calculator
+ */
+@Service
+public class Calculator {
+        //@Cacheable("sum")
+	int sum(int a, int b) {
+		return a + b;
+	} 
+}
